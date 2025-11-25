1b3383b779ee0c2986d930e51407b291a9574940
SteelFox113 <josemoyadezalol@gmail.com>
2025-11-25 02:54:46 +0000

    [🌙] Historial reiniciado 2025-11-25

---
+++ servidorminecraft/start.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# Cantidad de RAM (ajústala a lo que quieras usar)
+MEMORIA=4608M
+
+while true
+do
+  echo "Iniciando servidor de Minecraft..."
+  java -Xms$MEMORIA -Xmx$MEMORIA \
+  --add-modules=jdk.incubator.vector \
+  -XX:+UseG1GC \
+  -XX:+ParallelRefProcEnabled \
+  -XX:MaxGCPauseMillis=200 \
+  -XX:+UnlockExperimentalVMOptions \
+  -XX:+DisableExplicitGC \
+  -XX:+AlwaysPreTouch \
+  -XX:G1HeapWastePercent=5 \
+  -XX:G1MixedGCCountTarget=4 \
+  -XX:InitiatingHeapOccupancyPercent=15 \
+  -XX:G1MixedGCLiveThresholdPercent=90 \
+  -XX:G1RSetUpdatingPauseTimePercent=5 \
+  -XX:SurvivorRatio=32 \
+  -XX:+PerfDisableSharedMem \
+  -XX:MaxTenuringThreshold=1 \
+  -Dusing.aikars.flags=https://mcflags.emc.gs \
+  -Daikars.new.flags=true \
+  -XX:G1NewSizePercent=30 \
+  -XX:G1MaxNewSizePercent=40 \
+  -XX:G1HeapRegionSize=8M \
+  -XX:G1ReservePercent=20 \
+  -jar server.jar --nogui
+
+  echo "El servidor se ha detenido."
+  echo "Reiniciando en 10 segundos... (Ctrl+C para cancelar)"
+  sleep 10
+done
+
